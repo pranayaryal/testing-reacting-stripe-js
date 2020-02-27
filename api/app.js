46a037cad27c11b06a3e97cc2a116b2a7cf69182
pranay <drpranayaryal@gmail.com>
2020-02-26 19:40:32 -0500

    add api folder

--- api/app.js
+++ api/app.js
@@ -5,9 +5,10 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var testApiRouter = require('./routes/testApi');
+var apiRouter = require('./routes/api');
 
 var app = express();
 
@@ -24,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/api/intent', testApiRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
